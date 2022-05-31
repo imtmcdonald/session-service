caffc7cfc88919b2d4871474b4fc011173a15ee9
tim <timmc9104@gmail.com>
2022-05-31 10:10:59 -0400

    AB#29 - Initialize session microservice source code

---
+++ src/test/java/edu/psu/sweng894/chewsy/session/SessionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.psu.sweng894.chewsy.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
