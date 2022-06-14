c4455b8abd04ae8440873983e62a3055c7aeac67
tim <timmc9104@gmail.com>
2022-06-14 18:29:29 -0400

    AB#59 - Fixed unit test for class

--- src/test/java/edu/psu/sweng894/chewsy/session/infrastructure/ConciergeAPIRespositoryTests.java
+++ src/test/java/edu/psu/sweng894/chewsy/session/infrastructure/ConciergeAPIRespositoryTests.java
@@ -1,17 +1,22 @@
 package edu.psu.sweng894.chewsy.session.infrastructure;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
 import edu.psu.sweng894.chewsy.session.infrastructure.repository.ConciergeAPI.ConciergeAPIRespository;
 
+@WireMockTest(httpPort = 8089)
 public class ConciergeAPIRespositoryTests {
     private ConciergeAPIRespository classUnderTest;
 
@@ -25,7 +30,18 @@ public class ConciergeAPIRespositoryTests {
         final String location = "23666";
         final int radius = 5;
 
-        final JSONArray actual = classUnderTest.getRestaurants(location, radius);
+        JSONObject jo = new JSONObject();
+        jo.put("NAME", "Burger King");
+        jo.put("RATING", "3.4");
+        jo.put("LOCATION", "112 Jefferson Ave, Newport News VA 23601");
+        JSONArray expected = new JSONArray();
+        expected.put(jo);
+
+        stubFor(post("/find").willReturn(okJson(expected.toString())));
+
+        JSONArray actual = classUnderTest.getRestaurants(location, radius);
+
+        assertEquals(expected.toString(), actual.toString());
 
         assertNotNull(actual);
     }
